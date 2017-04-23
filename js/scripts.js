76f5e98d0166d75e43dfa3945863b4ff257e6fd4
Themalni <themalni@live.co.uk>
2017-04-23 17:45:37 +0200

    adding files

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+
+// on reset clear textarea
+function countCharacters(){
+  var text = document.querySelector("textarea");
+  var counter = document.querySelector(".counter");
+  var resetBtn = document.querySelector(".btn");
+  var maxTextLength = 250;
+  counter.innerHTML = maxTextLength;
+
+  function displayCounter(){
+    var textValue = text.value;
+    var counterRemain = maxTextLength - textValue.length;
+    if(textValue.length <= 239){
+      counter.innerHTML = "-" + counterRemain.toString();
+    }else if(textValue.length <= 249){
+      counter.style.color = "red";
+      counter.innerHTML = "-" + counterRemain.toString();
+    }else if(textValue.length == 250){
+      counter.style.color = "red";
+      counter.innerHTML = counterRemain.toString();
+      showWarning();
+    }
+  }
+  function showWarning(){
+    var message = document.querySelector(".app-warning");
+    message.style.opacity = 1;
+    message.style.transform = "translateY(0%) scale(1, 1)";
+  }
+  function hideWarning(){
+    var message = document.querySelector(".app-warning");
+    message.style.opacity = 0;
+    message.style.transform = "translateY(80%) scale(.8, .8)";
+  }
+  function reset(){
+    text.value = "";
+    counter.style.color = "#59b542";
+    counter.innerHTML = 250;
+    hideWarning();
+  }
+  text.addEventListener("input", displayCounter);
+  resetBtn.addEventListener("click", reset);
+}
+countCharacters();
